63f410cb73fa17bfcf8e306c7dd68803b81e033d
michalmendrek <michal.mendrek@gmail.com>
2019-09-15 12:36:06 +0200

    Working once

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/epoll.h>
 
 using namespace std;
 
@@ -15,17 +16,29 @@ int main()
 
 int inotifyFD = inotify_init();
 int inotifyFileWD;
+int epollFD   = epoll_create(sizeof(inotifyFD));
+int cfg;
+int ret;
 char buff[BUF_LEN];
 ssize_t numRead;
 char *p;
 struct inotify_event *event;
+struct epoll_event ev;
+
 
 if(inotifyFD == -1) {cout <<"inotify fault" << endl;}
 inotifyFileWD=inotify_add_watch(inotifyFD,"./plik.log",IN_CLOSE_WRITE);
 if(inotifyFileWD == -1) {cout <<"inotify fault" << endl; return 0;}
+if(epollFD <0 ) {cout << "Epoll fault" << endl;}
+
+ev.events=EPOLLIN|EPOLLOUT|EPOLLET;
+cfg = epoll_ctl(epollFD, EPOLL_CTL_ADD, inotifyFD, &ev);
+if (cfg < 0) {cout << "Epoll fault" << endl;}
+
 
 while(1)
 {
+/*
 numRead = read(inotifyFD, buff, BUF_LEN);
 if(numRead == 0){
   cout << "Read from inotify returned 0!" << endl;
@@ -43,6 +56,16 @@ for(p = buff; p < buff + numRead;)
  p+= sizeof(struct inotify_event) + event->len;
 }
 
+*/
+
+ ret = epoll_wait(epollFD, &ev, 100, 1000);
+ if(ret > 0 )
+ {
+  cout << "Modify done" << endl;
+ } else if(ret <0) {cout << "Error in pooling" << endl;} else 
+ {
+  cout << "TimeOut" << endl;
+ }
 
 }
 
